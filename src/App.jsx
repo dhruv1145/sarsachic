0bd403b9b418e3ccb414cdccd8a7455e75d21bfc
dhruv1145 <65786137+dhruv1145@users.noreply.github.com>
2024-06-16 17:00:30 +0000

    products page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header.jsx";
 import Hero from "./pages/Hero/Hero.jsx";
 import Login from "./pages/Login/Login.jsx";
 import SignUp from "./pages/SignUp/SignUp.jsx";
+import ProductPage from "./pages/ProductPage/ProductPage.jsx";
 
 export default function App() {
   return (
@@ -11,7 +12,8 @@ export default function App() {
       <Routes>
         <Route element={<Hero />} index />
         <Route element={<Login />} path="login" />
-        <Route element={<SignUp/>} path="signup"/>
+        <Route element={<SignUp />} path="signup" />
+        <Route element={<ProductPage />} path="products" />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Hero/Hero.jsx
+++ src/pages/Hero/Hero.jsx
@@ -1,26 +1,30 @@
 import React, { useState } from "react";
 import cover from "../../images/model2.png";
+import ProductPage from "../ProductPage/ProductPage";
 import "./Hero.css";
 
 const Hero = () => {
   return (
-    <div className="App">
-      <div className="leftPartition">
-        <div className="left-text">
-          <p className="description">
-            Transform your look with the latest in stree <br />
-            fashion. Urban vibes, unstoppable trends.
-          </p>
-          <button className="actionButton">SHOP NOW</button>
+    <div className="page">
+      <div className="App">
+        <div className="leftPartition">
+          <div className="left-text">
+            <p className="description">
+              Transform your look with the latest in stree <br />
+              fashion. Urban vibes, unstoppable trends.
+            </p>
+            <button className="actionButton">SHOP NOW</button>
+          </div>
+        </div>
+        <div className="rightPartition"></div>
+        <div className="model-img">
+          <img src={cover} />
+        </div>
+        <div className="logo-written">
+          <span className="logoText">SARSACHIC</span>
         </div>
       </div>
-      <div className="rightPartition"></div>
-      <div className="model-img">
-        <img src={cover} />
-      </div>
-      <div className="logo-written">
-        <span className="logoText">SARSACHIC</span>
-      </div>
+      <ProductPage />
     </div>
   );
 };
