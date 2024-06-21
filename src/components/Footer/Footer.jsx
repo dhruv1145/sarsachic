ed2036dbd71c53e5458d6bde1a01911e29701f73
NamanSriva <90003303+NamanSriva@users.noreply.github.com>
2024-06-21 16:19:35 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,38 @@
-import React , {useState} from "react";
+// src/Footer.js
+import React from 'react';
+import './footer.css'; // Import the CSS file
+import { FiMail } from "react-icons/fi";
+import { FaRegClock } from "react-icons/fa";
 
 const Footer = () => {
-    return ()
-}
+  return (
+    <div className="footer">
+      <div className="footer-section">
+        <h1>SARSACHIC</h1>
+        <p>Reach out to us</p>
+        <p className="footer-item">
+          <FiMail className="footer-icon" />
+          <a href="mailto:sarsa2025@gmail.com">sarsa2025@gmail.com</a>
+        </p>
+        <p className="footer-item">
+          <FaRegClock className="footer-icon" />
+          11am to 6pm Mon - Sun*
+        </p>
+      </div>
+      <div className="footer-section">
+        <h1>SHOP</h1>
+        <p><a href="#">WOMEN</a></p>
+        <p><a href="#">MEN</a></p>
+        <p><a href="#">ALL</a></p>
+      </div>
+      <div className="footer-section">
+        <h1>SUPPORT</h1>
+        <p><a href="#">Privacy policy</a></p>
+        <p><a href="#">Return</a></p>
+        <p><a href="#">Contact us</a></p>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
